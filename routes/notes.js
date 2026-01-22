b8a712bf476333a49f502d885d60a73a3a291c28
keito-2177 <keito21kk7@icloud.com>
2026-01-23 02:44:56 +0900

    final commit

--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
- // 接続情報を設定
+
+// 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://2201100303bj:Keito2177@cluster0.5olnsqp.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
-// データベース、コレクションを指定np
+// データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
